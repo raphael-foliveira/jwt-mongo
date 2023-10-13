5b579c3df1e30ea13a930f90699d8610aab47693
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-10-12 23:16:36 -0300

    fix: rename parseIdFromParams

--- internal/api/handlers/utils.go
+++ internal/api/handlers/utils.go
@@ -2,7 +2,7 @@ package handlers
 
 import "github.com/gofiber/fiber/v2"
 
-func parseObjectIdFromParams(c *fiber.Ctx) string {
+func parseIdFromParams(c *fiber.Ctx) string {
 	return c.Params("id")
 }
 
